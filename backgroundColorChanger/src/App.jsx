7daa4c2bd279c7d4eb55e758c12eeccb79bff244
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-20 08:29:53 +0530

    color changing successfully..

--- backgroundColorChanger/src/App.jsx
+++ backgroundColorChanger/src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+      const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+    <div className='w-full h-screen duration-200' style={{backgroundColor : color}}>
+        <div className='fixed flex flex-wrap bottom-20 justify-center inset-x-0 px-2'>
+           <div className='flex flex-wrap gap-5 bg-white py-2 px-4 rounded-xl'>
+           <button className='bg-red-700 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("red")}}>Red</button>
+           <button className='bg-green-600 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("green")}}>Green</button>
+           <button className='bg-orange-400 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("orange")}}>Orange</button>
+           <button className='bg-blue-700 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("blue")}}>Blue</button>
+           <button className='bg-red-400 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("salmon")}}>Cream</button>
+           <button className='bg-yellow-300 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("yellow")}}>Yellow</button>
+           <button className='bg-violet-500 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("violet")}}>Violet</button>
+           <button className='bg-indigo-900 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("indigo")}}>Indigo</button>
+           <button className='bg-black outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("black")}}>Black</button>
+           <button className='bg-pink-300 outline-none px-4 py-3 text-white rounded-xl' onClick={() => {setColor("pink")}}>pink</button>
+           </div>
+        </div>
+    </div>
+   </>
   )
 }
 
