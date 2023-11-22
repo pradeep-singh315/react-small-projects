441bc13311c8b8a7e489bca0e822298c0f47f9d8
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-22 09:04:56 +0530

    Set up successful..

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useCallback } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Pradeep Thakur</h1>
+    </>
+  )
+}
+
+export default App
