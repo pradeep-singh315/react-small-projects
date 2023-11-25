46be48172dd21b07e85070134704a19becaae3c9
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-25 15:43:35 +0530

    password generator work successfully..

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [ password, setPassword] = useState("")
 
+  const passwordRef = useRef(null)
+
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -25,6 +27,15 @@ function App() {
     } 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  
+
+  const copyPassword = useCallback(() =>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 100)
+    window.navigator.clipboard.writeText(password)
+    alert("Password Copied")
+  },[password])
+
   useEffect(() =>{
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
@@ -34,8 +45,8 @@ function App() {
       <div className='w-full max-w-md px-4 py-5 mx-auto rounded-lg text-white bg-slate-300 mt-52'>
         <div className='text-orange-500 text-xl font-extrabold pb-5'>Password Generator</div>
         <div className='flex shadow rounded-lg overflow-hidden mb-5'>
-          <input type="text" value={password} className='w-full outline-none p-2 rounded-lg text-orange-600' placeholder='Password' readOnly />
-          <button className='rounded-lg bg-blue-800 text-white px-2'>Copy</button>
+          <input type="text" value={password} className='w-full outline-none p-2 rounded-lg text-orange-600' placeholder='Password' readOnly ref={passwordRef}/>
+          <button className='rounded-lg bg-blue-800 text-white px-2' onClick={copyPassword}>Copy</button>
         </div>
         <div className='flex text-sm text-orange-600 gap-5'>
           <div>
